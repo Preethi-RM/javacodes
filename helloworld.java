0477b7b527916ebeda31222e0874890bf9ff8322
PreethirmManjunth <168974562+PreethirmManjunth@users.noreply.github.com>
2024-05-05 22:57:12 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 //In java control of execution will only starts from main method(entry point of a program)
-//If main method is not present the microprocessor unable to know where should it start to execute
+//If main method is not present the microprocessor unable to know where should it start to execute.
 class Demo{
   public static void main(String args[]){
     System.out.print("Hello World!")
